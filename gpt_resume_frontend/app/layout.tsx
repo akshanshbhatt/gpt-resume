c94fd5e3f3b4b3680907b90123dcdcfed6e5115c
Akshānsh <53227127+akshanshbhatt@users.noreply.github.com>
2024-02-25 18:32:51 +0530

    feat: add the initial frontend

---
+++ gpt_resume_frontend/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GPT-Resume",
+  description: "ATS using GPT models from OpenAI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
